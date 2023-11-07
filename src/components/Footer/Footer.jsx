6f4ce6596706838996d52ebe2b27ffe263219afe
SantoSarker101 <santosarker404@gmail.com>
2023-11-07 21:11:06 +0600

    create router and create MainLayout and install react bootstrap,react router dom

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div>
+			<h2 className='text-center'><small>&copy; of my chef recipes</small></h2>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div>
+			<div className="text-center">
+				<h2>Yummmy Food's Ocean</h2>
+				<p><small>We Provide American Chef Recipe</small></p>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
